94f2fbd615065f19d200eeadb4e1b224cf5039ca
Morgan Parry <m.parry@talentticker.ai>
2021-07-07 20:04:12 +0100

    fix: mobile toolbar

--- packages/editor/src/components/Editor/elements/PrettyDecentImages/PrettyDecentImageBtn.tsx
+++ packages/editor/src/components/Editor/elements/PrettyDecentImages/PrettyDecentImageBtn.tsx
@@ -8,20 +8,16 @@ import { resizeFile } from 'utils/resizeFile';
 export const PrettyDecentImageBtn = ({ children, ...props }: PrettyDecentButtonProps): JSX.Element => {
     const ref = useRef<HTMLInputElement>(null);
     const editor = useSlate();
-    // const { setAttachments } = usePrettyDecentAttachments();
-    const { onImage } = usePrettyDecentProps();
     const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files) {
             try {
                 const file = event.target.files[0];
                 const image = await resizeFile(file);
-                // onImage && onImage(image as File);
                 insertImage(editor, image as string);
+                ReactEditor.focus(editor);
             } catch (err) {
                 console.log(err);
             }
-            // setAttachments && setAttachments((ps) => [...ps, ...files] as File[]);
-            ReactEditor.focus(editor);
         }
     };
 
